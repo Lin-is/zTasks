5b6a498cb36f83be923cfada2baed38d10f47fa2
Polina <ipolina33@gmail.com>
2021-01-08 04:40:50 +0500

    everything is ready except for syntax highlighting

--- src/main/js/ui/control/EMail.js
+++ src/main/js/ui/control/EMail.js
@@ -16,6 +16,95 @@ Z8.define('org.zenframework.z8.template.controls.EMail', {
 
 });
 
+Z8.define('org.zenframework.z8.template.controls.xml', {
+	extend: 'Z8.form.field.TextArea',
+	scrollable: true,
+	readOnly: false,
+	
+	setContainersStyles: function(){
+		this.wrapper.style.overflowY = "auto";
+		this.wrapper.style.position = "absolute";
+		this.wrapper.style.width = "100%";
+		this.wrapper.style.height = "100%";
+		this.wrapper.style.top = "0";
+		this.wrapper.style.left = "0";
+		this.wrapper.style.zIndex = "0";
+		this.wrapper.style.font = "inherit";
+		this.pre.style.overflowY = "auto";
+		this.pre.style.position = "absolute";
+		this.pre.style.width = "100%";
+		this.pre.style.height = "100%";
+		this.pre.style.top = "0";
+		this.pre.style.left = "0";
+		this.pre.style.margin = "0";
+		this.pre.style.padding = "0";
+		this.pre.style.background = "transparent";
+		this.pre.style.font = "inherit";
+		this.code.style.overflowY = "auto";
+		this.code.style.position = "absolute";
+		this.code.style.width = "100%";
+		this.code.style.height = "100%";
+		this.code.style.top = "0";
+		this.code.style.left = "0";
+		this.code.style.font = "inherit";
+		this.code.style.padding= "0.14285714em";
+		this.code.style.whiteSpace = "pre-wrap";
+		this.code.style.wordWrap = "break-word";
+		this.code.style.background = "transparent";
+	},
+	
+	setMainStyles: function() {
+		this.elem.style.background = "transparent";
+		this.elem.style.zIndex = "1";
+		this.elem.style.color = "transparent";
+		this.elem.style.caretColor = "#222";
+		this.elem.style.overflowY = "auto";
+	},
+	
+	synchronizeScroll: function() {
+		var scroll = this.elem.scrollTop;
+		this.wrapper.scrollTop = scroll;
+		this.pre.scrollTop = scroll;
+		this.code.scrollTop = scroll;
+	},
+	
+	createContainers: function() {
+		if (!this.code){
+			this.wrapper = DOM.create("div", "wrapper");
+			this.pre = DOM.create("pre", "pre");
+			this.code = DOM.create("code", "code textarea control");
+			this.wrapper.append(this.pre);
+			this.pre.append(this.code);
+		} else {
+			return;
+		}
+	},
+	
+	validate: function() {
+		let text = this.getValue();
+		if (this.code) {
+			text = text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+			this.code.innerHTML = text;
+		}
+	},
+	
+	completeRender: function() {
+		Z8.form.field.TextArea.prototype.completeRender.call(this);
+
+		this.elem = DOM.selectNode(this.getCls());
+		this.elem.setAttribute("scrollable", "scrollable");
+		if (!this.code) {
+			this.createContainers();
+		}
+		if (this.elem && this.wrapper && this.elem.parentElement.children.length < 3) {
+			this.elem.parentElement.insertBefore(this.wrapper, this.elem);
+			this.setMainStyles();
+			this.setContainersStyles();
+			this.elem.addEventListener("scroll", this.synchronizeScroll.bind(this));
+		}
+	}
+});
+
 Z8.define('org.zenframework.z8.template.controls.Youtube', {
 	extend: 'Z8.form.field.Text',
 	
@@ -30,8 +119,11 @@ Z8.define('org.zenframework.z8.template.controls.Youtube', {
 	},
 	
 	renderVideo: function(value) {
-		let container = DOM.create("div", "row embed-responsive embed-responsive-16by9");
-		this.video = DOM.create("iframe", "embed-responsive-item");
+		let container = DOM.create("div", "row");
+		this.video = DOM.create("iframe", "video col-lg-6 col-md-6 col-sm-6 cell");
+		this.video.setAttribute("frameborder", "0");
+		this.video.allow = "accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+		this.video.setAttribute("allowfullscreen", "allowfullscreen");
 		container.append(this.video);
 		this.updateVideo(value);
 		return container;
@@ -66,97 +158,6 @@ Z8.define('org.zenframework.z8.template.controls.Youtube', {
 	},
 });
 
-Z8.define('org.zenframework.z8.template.controls.xml', {
-	extend: 'Z8.form.field.TextArea',
-	scrollable: true,
-	readOnly: false,
-	
-	setContainersStyles: function(){
-		this.wrapper.style.overflow = "hidden";
-		this.wrapper.style.position = "absolute";
-		this.wrapper.style.width = "100%";
-		this.wrapper.style.height = "100%";
-		this.wrapper.style.top = "0";
-		this.wrapper.style.left = "0";
-		this.wrapper.style.zIndex = "0";
-		this.wrapper.style.font = "inherit";
-		this.pre.style.overflow = "hidden";
-		this.pre.style.position = "absolute";
-		this.pre.style.width = "100%";
-		this.pre.style.height = "100%";
-		this.pre.style.top = "0";
-		this.pre.style.left = "0";
-		this.pre.style.margin = "0";
-		this.pre.style.padding = "0";
-		this.pre.style.background = "transparent";
-		this.pre.style.font = "inherit";
-		this.code.style.overflow = "hidden";
-		this.code.style.position = "absolute";
-		this.code.style.width = "100%";
-		this.code.style.height = "100%";
-		this.code.style.top = "0";
-		this.code.style.left = "0";
-		this.code.style.font = "inherit";
-		this.code.style.padding= "0.14285714em";
-		this.code.style.whiteSpace = "pre-wrap";
-		this.code.style.wordWrap = "break-word";
-		this.code.style.background = "transparent";
-	},
-	
-	setMainStyles: function() {
-		this.elem.style.background = "transparent";
-		this.elem.style.zIndex = "1";
-		this.elem.style.color = "transparent";
-		this.elem.style.caretColor = "#222";
-	},
-	
-	createContainers: function() {
-		if (!this.code){
-			this.wrapper = DOM.create("div", "wrapper");
-			this.pre = DOM.create("pre", "pre");
-			this.code = DOM.create("code", "code textarea control");
-			this.wrapper.append(this.pre);
-			this.pre.append(this.code);
-		} else {
-			return;
-		}
-	},
-	
-	validate: function() {
-		
-		let text = this.getValue();
-
-		if (this.code) {
-			text = text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
-			this.code.innerHTML = text;
-		}
-	},
-	
-	completeRender: function() {
-		Control.prototype.completeRender.call(this);
-		
-		DOM.on(this, 'click', this.onClick, this);
-		DOM.on(this, 'keyDown', this.onKeyDown, this);
-
-		var input = this.input = this.selectNode(this.getInputTag() + '[name=' + this.getInputName() + ']');
-
-		if(this.editor)
-			DOM.on(input, 'input', this.onInput, this);
-
-		
-		this.elem = DOM.selectNode(this.getCls());
-		if (!this.code) {
-			this.createContainers();
-		}
-		if (this.elem && this.wrapper && this.elem.parentElement.children.length < 3) {
-			this.elem.parentElement.insertBefore(this.wrapper, this.elem);
-			this.setMainStyles();
-			this.setContainersStyles();
-		}
-	}
-
-});
-
 Z8.define('org.zenfraemwork.z8.template.controls.audio', {
 	extend: 'Z8.form.field.Text',
 	tag: 'audio',
@@ -169,12 +170,18 @@ Z8.define('org.zenfraemwork.z8.template.controls.audio', {
 	getAudio: function(e) {
 		let player = document.querySelector(".audioPlayer");
 		var file = e.target.files[0];
-		var reader = new FileReader();
-		reader.onload = function(evt) {
-		    player.src = evt.target.result;
-			player.load();
-		};
-		reader.readAsDataURL(file);
+		console.log("file", file);
+		if (file) {
+			var reader = new FileReader();
+			reader.onload = function(evt) {
+			    player.src = evt.target.result;
+				player.load();
+			};
+			reader.readAsDataURL(file);
+			console.log("readed");
+		} else {
+			return;
+		}
 	},
 	
 	createPlayer: function() {
@@ -184,14 +191,17 @@ Z8.define('org.zenfraemwork.z8.template.controls.audio', {
 	
 	completeRender: function() {
 		Z8.form.field.Text.prototype.completeRender.call(this);
-		let box = this.getBox();
+		var box = this.getBox();
 		box.firstElementChild.remove();
 		if (!this.audio) {
 			this.createPlayer();
 		}
 		box.appendChild(this.audio);
 		
-		let input = this.findInput();
+		var input = this.findInput();
 		input.addEventListener("change", this.getAudio);
+		
+		var event = new Event('change', { bubbles: true });
+		input.dispatchEvent(event);
 	}
 	});
